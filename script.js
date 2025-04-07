92785fe65a1151f6efddb7342a77368d21f740bb
mpadron1976 <misti@thefdlgroup.com>
2025-04-07 16:59:37 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('contact-form').addEventListener('submit', function(e) {
+ e.preventDefault();
+ alert('Message submitted! (You can set this up to send to an email or server.)');
+});
