986b0a853f2c3038535f1ed2d434a3af547d3e7f
mpadron1976 <misti@thefdlgroup.com>
2025-04-08 17:55:55 -0500

    Delete IndigoBogusAutoresponder directory
    In Open Repository

--- IndigoBogusAutoresponder/script.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('contact-form').addEventListener('submit', function(e) {
- e.preventDefault();
- alert('Message submitted! (You can set this up to send to an email or server.)');
-});
